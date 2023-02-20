90e01c414823d80ec936af730bcfc0dfa43d5c6d
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2023-02-20 03:28:01 +0100

    CodeChallenge
    Explained why / How I used (Get-Date).Date

---
+++ MicrosoftChallengeCodePS/WriteYourFirstPowerShellCode.ps1
@@ -0,0 +1,6 @@
+# instead of prompting the user, I used Get-Command -Noun *date* -Verb Get*, pipelined with Get-Member then used .Date to get just date
+$Date = (Get-Date).Date
+$Name = Read-Host -Prompt "Please provde your name"
+
+Write-host "Today's Date is $Date"
+Write-Host "Today is the day $Name began a powershell programming journey"
